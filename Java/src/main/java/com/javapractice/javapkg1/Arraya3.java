54a03e7c49a773119991f95489f126ac258f0814
UdayKumarAJ <udaykumaraj@gmail.com>
2021-01-14 13:30:10 +0530

    added

---
+++ Java/src/main/java/com/javapractice/javapkg1/Arraya3.java
@@ -0,0 +1,8 @@
+package com.javapractice.javapkg1;
+/*
+ * write a program to print the first pair of numbers whose sum is 10
+ * a=[] = {2,4,8,4,9,6} == 2+8=10(true)  and do not print 4+6=10(false)
+ */
+public class Arraya3 {
+
+}
