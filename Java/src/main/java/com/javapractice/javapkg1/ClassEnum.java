f8ce443d6bedbeae7038ad3fd8f2b1f48f054232
UdayKumarAJ <udaykumarajgmail.com>
2021-02-16 11:03:25 +0530

    enum update

---
+++ Java/src/main/java/com/javapractice/javapkg1/ClassEnum.java
@@ -0,0 +1,21 @@
+package com.javapractice.javapkg1;
+
+import com.javapractice.javapkg1.Coffee.coffeeSize;
+//Enums restrict a variable to have one of only a few predefined values. 
+//The values in this enumerated list are called enums
+class Coffee {
+	enum coffeeSize {
+		small, medium, large
+	};
+
+	coffeeSize size;
+
+}
+
+public class ClassEnum {
+	public static void main(String[] args) {
+		Coffee c = new Coffee();
+		c.size = coffeeSize.medium;
+		System.out.println("Coffee Size : "+ c.size);
+	}
+}
--- Java/src/main/java/com/javapractice/javapkg1/ReverseString.java
+++ Java/src/main/java/com/javapractice/javapkg1/ReverseString.java
@@ -2,6 +2,9 @@ package com.javapractice.javapkg1;
 
 public class ReverseString {
 
+	 static String str1 = "world";
+	 static String str2 = "WORLD";
+	
 	public static String reverseStringLogic(String s) {
 		char[] array = s.toCharArray();
 		String rev = "";
@@ -10,10 +13,19 @@ public class ReverseString {
 		}
 		return rev;
 	}
+	
+	public static void compare(String s1, String s2)
+	{
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareToIgnoreCase(s2));
+	}
 
 	public static void main(String[] args) {
 		String str = "one";
 		System.out.println(reverseStringLogic(str));
-
+		
+		compare(str1, str2);
+		
+		
 	}
 }
